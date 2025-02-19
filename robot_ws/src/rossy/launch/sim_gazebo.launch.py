9b85323e2fcbda368f801f4fee8037fd0e9aaeb0
Gerard Casa <03gerardfranch@gmail.com>
2025-02-19 22:17:04 +0100

    Millore varies, creació d'un launch file per Gazebo, un archiu de llançament per aquest i un archiu pont per la nova versió

---
+++ robot_ws/src/rossy/launch/sim_gazebo.launch.py
@@ -0,0 +1,49 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+
+from launch_ros.actions import Node
+
+
+
+def generate_launch_description():
+
+    archivo_launch = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('rossy'),'launch','archivo.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
+
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
+                    launch_arguments={'gz_args': 'empty.sdf', 'on_exit_shutdown': 'true'}.items()  
+             )
+
+    generar_robot = Node(package='ros_gz_sim', executable='create',
+                        arguments=['-topic', 'robot_description',
+                                   '-name', 'rossy',
+                                   '-z', '0.1'],
+                        output='screen')
+    
+    parametros_bridge = os.path.join(get_package_share_directory('rossy'),'configuracions','gz_bridge.yaml')
+    ros_gz_bridge = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        arguments=[
+            '--ros-args',
+            '-p',
+            f'config_file:={parametros_bridge}',
+        ]
+    )
+
+    return LaunchDescription([
+        archivo_launch,
+        gazebo,
+        generar_robot,
+        ros_gz_bridge,
+    ])
