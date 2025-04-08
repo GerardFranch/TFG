980e3809ee3fec6ab482ef15d4f94fdcba164da9
Gerard Casa <03gerardfranch@gmail.com>
2025-04-08 15:14:26 +0200

    Rebre correctament velocitat en rev/s

--- motor_controller/proba_rebre_encoders.ino/proba_rebre_encoders.ino.ino
+++ motor_controller/proba_rebre_encoders.ino/proba_rebre_encoders.ino.ino
@@ -15,7 +15,7 @@ void setup() {
   pinMode(in2, OUTPUT);
   pinMode(left_enc_B, INPUT);
 
-  attachInterrupt(digitalWrite(left_enc_A),leftEncoderCallback,RISING);
+  attachInterrupt(digitalPinToInterrupt(left_enc_A),leftEncoderCallback,RISING);
   
   digitalWrite(in1,HIGH);
   digitalWrite(in2,LOW );
@@ -25,8 +25,12 @@ void setup() {
 }
 
 void loop() {
-  left_wheel_vel = 10*left_encoder_counter + (60.0/858.0);
+  left_wheel_vel = 10*left_encoder_counter + (60.0/858.0)*0.10472; //11senayals per volta x 78 ratio reduccio = 858 ---- 1rpm = 0.10472 rad/s
+  String encoder_read = left_encoder_sign + String(left_wheel_vel);
+  Serial.println(encoder_read);
   analogWrite(enA, 100);
+
+  left_encoder_counter = 0;
   delay(100);
 }
 
